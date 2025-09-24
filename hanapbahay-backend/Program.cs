a0c056d740d292a4019aae1abc5a6d9fd6390d60
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-09-24 11:48:07 +0800

    Add docker support

--- hanapbahay-backend/Program.cs
+++ hanapbahay-backend/Program.cs
@@ -1,3 +1,6 @@
+using hanapbahay_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +12,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
