0112a1b086bef8082a7c9d6316b8502b1a96953b
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-09-25 17:39:52 +0800

    Uninstalled automapper DI package

--- hanapbahay-backend/Program.cs
+++ hanapbahay-backend/Program.cs
@@ -3,6 +3,7 @@ using hanapbahay_backend.Models.Entities;
 using hanapbahay_backend.Services.Auth;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,7 +24,11 @@ builder.Services.AddIdentityApiEndpoints<User>()
 
 builder.Services.AddScoped<IAuthService, AuthService>();
 
-builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.LicenseKey = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ikx1Y2t5UGVubnlTb2Z0d2FyZUxpY2Vuc2VLZXkvYmJiMTNhY2I1OTkwNGQ4OWI0Y2IxYzg1ZjA4OGNjZjkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2x1Y2t5cGVubnlzb2Z0d2FyZS5jb20iLCJhdWQiOiJMdWNreVBlbm55U29mdHdhcmUiLCJleHAiOiIxNzkwMjk0NDAwIiwiaWF0IjoiMTc1ODc5MjE1NiIsImFjY291bnRfaWQiOiIwMTk5ODAyOGNiZDc3MjZlODg0NTA2YjFhOTY1NjdkNCIsImN1c3RvbWVyX2lkIjoiY3RtXzAxazYwMngxc2ZjNXpkOHhmZ20wYXgybjJtIiwic3ViX2lkIjoiLSIsImVkaXRpb24iOiIwIiwidHlwZSI6IjIifQ.YJ30oZOynBeL_vU1pGtW6g8Xb7-Fm9pEXLwF6RR1MG5IUv6I8CslmfE-D47xwdAS3X7O0nwuTANLYCg64A8ot8AxhBO1M26GdwDyUDeEw0dZQDW3K2IaZXKhsK-3y4m38w9_Sw6UdD7gj7b0IXxHcUbPyVEgvwzIyx9ClNwUDDHkrhkb_oEhpHOC1ehYTxia4U4frV3TE_XyIezgILc5YZJkRjBB3i5nzvPxIBSJ1bEAZ-N80RGPhvFPO_Xal-PIGIVS-_oJUFeNI8FKJgPZDXnaov2AC3F5TqtMorn7FDJrXlYPWEVwPYwuYrkCNmC79YyacInzZmY2jOWJNOmM7A";
+    cfg.AddMaps(typeof(Program));
+});
 
 var app = builder.Build();
 
