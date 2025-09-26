8763fdbd3a7db5845a093940ab4bdff9c19bb261
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-09-26 13:26:11 +0800

    Added a logger

--- hanapbahay-backend/Controllers/AuthController.cs
+++ hanapbahay-backend/Controllers/AuthController.cs
@@ -10,10 +10,12 @@ namespace hanapbahay_backend.Controllers;
 public class AuthController : ControllerBase
 {
     private readonly IAuthService _authService;
+    private readonly ILogger<AuthController> _logger;
 
-    public AuthController(IAuthService authService)
+    public AuthController(IAuthService authService, ILogger<AuthController> logger)
     {
         _authService = authService;
+        _logger = logger;
     }
 
     [HttpPost("register")]
@@ -22,7 +24,13 @@ public class AuthController : ControllerBase
     {
         var (success, errors) = await _authService.RegisterAsync(request);
         if (!success)
+        {
+            foreach (var error in errors)
+            {
+                _logger.LogWarning("Error:" + error);
+            }
             return BadRequest(new { errors });
+        }
 
         return Ok(new { message = "User registered successfully", role = request.Role });
     }
@@ -33,7 +41,10 @@ public class AuthController : ControllerBase
     {
         var (success, role) = await _authService.LoginAsync(request);
         if (!success)
+        {
+            _logger.LogWarning("Invalid credentials");
             return Unauthorized(new { message = "Invalid credentials" });
+        }
 
         return Ok(new { message = "Login successful", role });
     }
