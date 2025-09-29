29ddfa100d88f3e409304c2b848f9728aea9889a
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-09-29 15:59:40 +0800

    Add getting SAS URL

--- hanapbahay-backend/Services/Storage/BlobStorageService.cs
+++ hanapbahay-backend/Services/Storage/BlobStorageService.cs
@@ -1,5 +1,6 @@
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
 
 public class BlobStorageService : IBlobStorageService
 {
@@ -39,5 +40,27 @@ public class BlobStorageService : IBlobStorageService
             return response.Value.Content;
         }
         return null;
+    }
+
+    public string GetSasUrl(string fileName, string containerName, TimeSpan validFor)
+    {
+        var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+        var blobClient = containerClient.GetBlobClient(fileName);
+
+        if (!blobClient.CanGenerateSasUri)
+            throw new InvalidOperationException("SAS generation not permitted. Ensure you are using a key credential.");
+
+        var sasBuilder = new BlobSasBuilder
+        {
+            BlobContainerName = containerName,
+            BlobName = fileName,
+            Resource = "b", // b = blob
+            ExpiresOn = DateTimeOffset.UtcNow.Add(validFor)
+        };
+
+        sasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+        var sasUri = blobClient.GenerateSasUri(sasBuilder);
+        return sasUri.ToString();
     }
 }
--- hanapbahay-backend/Services/Storage/IBlobStorageService.cs
+++ hanapbahay-backend/Services/Storage/IBlobStorageService.cs
@@ -3,4 +3,5 @@ public interface IBlobStorageService
     Task<string> UploadFileAsync(Stream fileStream, string fileName, string containerName);
     Task<bool> DeleteFileAsync(string fileName, string containerName);
     Task<Stream?> DownloadFileAsync(string fileName, string containerName);
+    string GetSasUrl(string fileName, string containerName, TimeSpan validFor);
 }
