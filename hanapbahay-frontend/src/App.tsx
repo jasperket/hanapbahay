c907da838eda45d40ebd9bc18ec93b8b3a33116e
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-10-01 00:49:13 +0800

    Setup react query

--- hanapbahay-frontend/src/App.tsx
+++ hanapbahay-frontend/src/App.tsx
@@ -2,14 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import "@fontsource-variable/inter";
 import Home from "./pages/Home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
