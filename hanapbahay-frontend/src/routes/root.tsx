554240623192806d0f6fcfea65aa45c7db1127d7
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-10-10 16:40:53 +0800

    feat: add react query

--- hanapbahay-frontend/src/routes/root.tsx
+++ hanapbahay-frontend/src/routes/root.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router";
 import { Toaster } from "sonner";
 import { AuthProvider } from "@/providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const RootLayout = () => {
   return (
-    <AuthProvider>
-      <Outlet />
-      <Toaster richColors position="top-center" />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Outlet />
+        <Toaster richColors position="top-center" />
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 
